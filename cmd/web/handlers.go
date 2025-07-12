e6abd3d8ce343fdf16f50c6b670128f549c70dae
ili14 <ilidevloper@gmail.com>
2025-07-12 17:55:41 +0330

    1404/04/21 - 17:55

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,3 +1,4 @@
+// handlers.go
 package main
 
 import (
@@ -37,8 +38,11 @@ func home(w http.ResponseWriter, r *http.Request) {
 		http.NotFound(w, r)
 		return
 	}
-
-	ts, err := template.ParseFiles("./ui/html/pages/home.tmpl")
+	// Use the template.ParseFiles() function to read the template file into a
+	// template set. If there's an error, we log the detailed error message and use
+	// the http.Error() function to send a generic 500 Internal Server Error
+	// response to the user.
+	ts, err := template.ParseFiles("./ui/html/pages/home.html")
 	if err != nil {
 		log.Print(err.Error())
 		http.Error(w, "Internal Server Error", 500)
